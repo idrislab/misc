ed8506fa44ccc35d039f43d96ea9e4c5374e63d2
Luis Pitta Gros <luispittagros@Luis-MBP.home>
2017-02-15 18:37:37 +0000

    catch exception and log

--- app/Crawler/ThreadCrawler.php
+++ app/Crawler/ThreadCrawler.php
@@ -3,6 +3,7 @@
 namespace App\Crawler;
 
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\DomCrawler\Crawler as DomCrawler;
 
 /**
@@ -43,45 +44,51 @@ class ThreadCrawler
             $crawler = $this->crawler->get($this->getUrl($thread, $page));
 
             if ($page === 1) {
-                if($crawler->filter('#pagination_top a.popupctrl')->count()) {
+                if ($crawler->filter('#pagination_top a.popupctrl')->count()) {
                     $pages = strtolower($crawler->filter('#pagination_top a.popupctrl')->text());
                     $pages = substr($pages, strpos($pages, 'of ') + strlen('of '));
                     $this->pages = ((int)$pages >= $this->pages) ? $this->pages : (int)$pages;
-                } else  {
+                } else {
                     $this->pages = 1;
                 }
             }
 
-            if(!$crawler->filterXpath('//li[contains(@id,\'post_\')]')->count()) {
+            if (!$crawler->filterXpath('//li[contains(@id,\'post_\')]')->count()) {
                 //thread was deleted
                 continue;
             }
 
             $data = $crawler->filterXpath('//li[contains(@id,\'post_\')]')->each(function (DomCrawler $node) {
-                $postDate = $node->filter('.date')->text();
-                $postDate = trim(preg_filter(['/[\s,]+/', '/-/'], [' ', '/'], $postDate));
+                try {
+                    $postDate = $node->filter('.date')->text();
+                    $postDate = trim(preg_filter(['/[\s,]+/', '/-/'], [' ', '/'], $postDate));
 
-                $joinDate = $node->filterXpath('//div[@class="userinfo"]//dl/dt[contains(text(),\'Join Date:\')]')->text();
-                $joinDate = trim(str_replace('Join Date:', '', $joinDate));
+                    $joinDate = $node->filterXpath('//div[@class="userinfo"]//dl/dt[contains(text(),\'Join Date:\')]')->text();
+                    $joinDate = trim(str_replace('Join Date:', '', $joinDate));
 
-                $posts = $node->filterXpath('//div[@class="userinfo"]//dl/dt[contains(text(),\'Posts:\')]')->text();
-                $posts = trim(preg_replace(['/,/', '/Posts\:/'], ['', ''], $posts));
+                    $posts = $node->filterXpath('//div[@class="userinfo"]//dl/dt[contains(text(),\'Posts:\')]')->text();
+                    $posts = trim(preg_replace(['/,/', '/Posts\:/'], ['', ''], $posts));
 
-                $repPower = $node->filterXpath('//div[@class="userinfo"]//dl/dt[contains(text(),\'Rep Power:\')]')->text();
-                $repPower = trim(str_replace('Rep Power:', '', $repPower));
+                    $repPower = $node->filterXpath('//div[@class="userinfo"]//dl/dt[contains(text(),\'Rep Power:\')]')->text();
+                    $repPower = trim(str_replace('Rep Power:', '', $repPower));
 
-                $post = $node->filter('.postcontent')->html();
+                    $post = $node->filter('.postcontent')->html();
 
-                $avatar = $node->filter('.postuseravatar img')->attr('src');
+                    $avatar = $node->filter('.postuseravatar img')->attr('src');
 
-                return [
-                    'postDate' => new Carbon($postDate),
-                    'joinDate' => new Carbon($joinDate),
-                    'posts' => $posts,
-                    'repPower' => $repPower,
-                    'avatar' => $avatar,
-                    'post' => $post,
-                ];
+                    return [
+                        'postDate' => new Carbon($postDate),
+                        'joinDate' => new Carbon($joinDate),
+                        'posts' => $posts,
+                        'repPower' => $repPower,
+                        'avatar' => $avatar,
+                        'post' => $post,
+                    ];
+
+                } catch (\Exception $e) {
+                    Log::warning($e->getMessage());
+                    return;
+                }
             });
 
             $posts = array_merge($posts, $data);
