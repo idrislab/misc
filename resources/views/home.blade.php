06081156f94ac6d293322bfbe33e022ac4cf9fba
Luis Pitta Gros <luispittagros@Luis-MBP.home>
2017-02-16 14:39:11 +0000

    make images responsive

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -143,5 +143,11 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"
         integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn"
         crossorigin="anonymous"></script>
+
+<script>
+    $(function () {
+        $('img').attr('class', 'img-fluid');
+    });
+</script>
 </body>
 </html>
