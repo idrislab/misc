12f2489e6a2dacd35c339efee01ff5fa287a11ae
Luis Pitta Gros <luispittagros@Luis-MBP.home>
2017-02-16 22:00:21 +0000

    change views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -90,13 +90,24 @@
             background-color: #fff;
         }
 
-        li.active {
+        .navbar li.active {
             background-color: #12466D;
-            color: #fff;
         }
 
-        li.active a {
+        .navbar li:hover:not(.active) {
+            background-color: #12466D;
+        }
+
+        .navbar li:not(.active):hover a {
             color: #fff !important;
+        }
+
+        .navbar li.active a {
+            color: #fff !important;
+        }
+
+        .navbar li a {
+            color: #424242;
         }
 
         .pagination {
@@ -106,35 +117,8 @@
 </head>
 <body>
 <div class="container-fluid">
-    <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
-                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <a class="navbar-brand" href="#">
-            misQ.me
-        </a>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item {{ \Route::currentRouteName() == 'hot' ? 'active' : '' }}">
-                    <a class="nav-link" href="/">Hot</a>
-                </li>
-                <li class="nav-item {{ \Route::currentRouteName() == 'best' ? 'active' : '' }}">
-                    <a class="nav-link" href="/best">Best</a>
-                </li>
-            </ul>
-        </div>
-        <div class="col-md-2">
-            <span class="last-update">Updated </span> <span
-                    class="time">{{ \Carbon\Carbon::parse($status->updated_at)->diffForHumans() }}</span>
-        </div>
-    </nav>
-
-    <div id="accordion" class="lazy col-md-12" data-loader="ajaxLoader" role="tablist" aria-multiselectable="true">
-        @include('threads')
-    </div>
+    @include('navbar')
+    @include('threads')
 </div>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"
---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">
+        misQ.me
+    </a>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item {{ \Route::currentRouteName() == 'hot' ? 'active' : '' }}">
+                <a class="nav-link pl-3 pr-3" href="/">Trending</a>
+            </li>
+            <li class="nav-item {{ \Route::currentRouteName() == 'best' ? 'active' : '' }}">
+                <a class="nav-link pl-3 pr-3" href="/best">Best</a>
+            </li>
+        </ul>
+    </div>
+    <div class="col-md-3  text-right">
+        <span class="last-update">Updated </span> <span
+                class="time">{{ \Carbon\Carbon::parse($status->updated_at)->diffForHumans() }}</span>
+    </div>
+</nav>
